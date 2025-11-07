64b812f6bc5e878e349aa3a64030572b831d797b
Dashkevich Andrey <dashkevich@ittest-team.ru>
2025-11-07 20:33:31 +0700

    change: generate only for directories where has arb directory

---
+++ example/lib/feature/navigation/widget/navigation.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+final class Navigation extends StatelessWidget {
+  const Navigation({super.key});
+
+  @override
+  Widget build(BuildContext context) => const Placeholder();
+}
